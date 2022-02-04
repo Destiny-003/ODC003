4fac4eb1ef21f6f20cffd270ebb528306acb3926
Isa Ali <88540620+Destiny-003@users.noreply.github.com>
2022-02-04 00:55:06 -0800

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,45 @@
+
+var can = document.getElementById("can");
+
+function pc() {
+    can.innerHTML =`<div style="text-align:center; ">  بسم الله الرحمن الرحيم </div>  <p> يا الله يا رحمن يا رحيم</p>`;
+}
+function cmd() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function registry() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function task() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function telegram() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function vars() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function word() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function excel() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function html() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function css() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function visualB() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function cpp() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function java() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function design() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
