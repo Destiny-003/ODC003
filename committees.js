76905c0cf09f10c9d6b604549accea999aacb03c
Isa Ali <88540620+Destiny-003@users.noreply.github.com>
2022-02-04 00:56:16 -0800

    Add files via upload

---
+++ committees.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("legislative").style.width = "100%";
+    document.getElementById("lmain").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("legislative").style.width = "0";
+    document.getElementById("lmain").style.marginLeft= "0";
+  }
