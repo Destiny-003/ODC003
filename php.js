5158adec2c552783afa551555a1d5ba4a7801970
Isa Ali <88540620+Destiny-003@users.noreply.github.com>
2022-02-04 00:58:49 -0800

    Add files via upload

---
+++ php.js
@@ -0,0 +1,55 @@
+var can = document.getElementById("can");
+var con = document.getElementById("con");
+can.style.height = "128em"
+con.style.height = "128em"
+function basic() {
+    can.innerHTML =`<div style="text-align:center; ">  بسم الله الرحمن الرحيم </div>  <p> يا الله يا رحمن يا رحيم</p>`;
+}
+function ifElse() {
+    can.innerHTML ="<div> php lang </div>";
+}
+function swit() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function loops() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function array() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function string() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function webC() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function getPost() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function fileIn() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function fileIO() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function functions() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function cookies() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function sessions() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function sendingEmail() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function fileUp() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function superGl() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
+function regEx() {
+    can.innerHTML ="<div> بسم الله الرحمن الرحيم </div>";
+}
