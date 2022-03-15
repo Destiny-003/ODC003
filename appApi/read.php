2e72d4f9432e1778126572f303ffab7d4fa34b97
Isa Ali <88540620+Destiny-003@users.noreply.github.com>
2022-03-15 17:22:18 +0300

    Update read.php

--- appApi/read.php
+++ appApi/read.php
@@ -5,9 +5,9 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
-$username = "epiz_31065689";
-$password = "skHFVFf6PCFmpNz";
-$database = new  PDO("mysql:host=sql105.epizy.com; dbname=epiz_31065689_MainDB;charset=latin1;", $username, $password);
+    $username = "sql11479227";
+    $password = "QWBWkbTlBC";
+    $database = new  PDO("mysql:host=sql11.freemysqlhosting.net; dbname=sql11479227;charset=latin1;", $username, $password);
 
 $getData = $database->prepare("SELECT * FROM roozchat");
 $getData->execute();
