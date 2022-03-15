24bf249e50ce172aa4b518941dc8267fc2da682f
Isa Ali <88540620+Destiny-003@users.noreply.github.com>
2022-03-15 15:07:50 +0300

    Add files via upload

---
+++ lab.js
@@ -0,0 +1,29 @@
+
+
+
+setInterval(()=>{
+    fetch("http://localhost/api/rooz/read.php").then(response => response.json())
+    .then(data=>{
+        data.reverse();
+        let main = document.getElementById("content");
+        main.innerHTML = '';
+        data.forEach(element => {
+        main.innerHTML += "<div class='bg-msg'>" + "<p class='message'>" + "<b style = 'color:blue;text-shadow:1px 1px 1px green'>" + element.user  + ": " + "</b>" + element.messages + "</p>" + "<div>";
+    });
+    
+    });
+    },1000);
+    
+    function clear() {
+        let clean = document.getElementById("clean");
+        clean.innerHTML ="";
+    }
+    function inter() {
+        let cover =document.getElementById("cover");
+        let inter =document.getElementById("inter");
+        let move =document.getElementById("move");
+            inter.style.display = "block";
+            move.style.display = "none";
+    }
+    
+    
---
+++ rooz.js
@@ -0,0 +1,214 @@
+let online = document.getElementById("online");
+let theme = document.getElementById("theme");
+let body = document.getElementById("body");
+let users = document.getElementById("users");
+let main = document.getElementById("main");
+let lab = document.getElementById("lab");
+let setLab = document.getElementById("setLab");
+let cover1 = document.getElementById("cover1");
+let cover2 = document.getElementById("cover2");
+let b1 = document.getElementById("b1");
+let b2 = document.getElementById("b2");
+let b3 = document.getElementById("b3");
+let b4 = document.getElementById("b4");
+let b5 = document.getElementById("b5");
+let b6 = document.getElementById("b6");
+let b7 = document.getElementById("b7");
+let ba = document.getElementById("ba");
+let bb = document.getElementById("bb");
+let bc = document.getElementById("bc");
+let text = document.getElementById("text");
+
+function themes() {
+    theme.style.display ="block"; 
+}
+
+function planets() {
+    online.style.color = "aliceblue";
+    body.style.backgroundColor = "black";
+    users.style.backgroundColor = "black";
+    users.style.borderColor = "rgb(47, 65, 82)"
+    main.style.backgroundColor = "rgb(47, 65, 82)";
+    lab.style.backgroundColor = "black";
+    setLab.style.backgroundColor = "black";
+    cover1.style.backgroundColor = "rgb(47, 65, 82)";
+    cover2.style.backgroundColor = "rgb(47, 65, 82)";
+    b1.style.backgroundColor = "rgb(47, 65, 82)";
+    b2.style.backgroundColor = "rgb(47, 65, 82)";
+    b3.style.backgroundColor = "rgb(47, 65, 82)";
+    b4.style.backgroundColor = "rgb(47, 65, 82)";
+    b5.style.backgroundColor = "rgb(47, 65, 82)";
+    b6.style.backgroundColor = "rgb(47, 65, 82)";
+    b7.style.backgroundColor = "rgb(47, 65, 82)";
+    ba.style.backgroundColor = "rgb(47, 65, 82)";
+    bb.style.backgroundColor = "rgb(47, 65, 82)";
+    bc.style.backgroundColor = "rgb(47, 65, 82)";
+    theme.style.backgroundColor = "rgb(47, 65, 82)";  
+
+}
+
+function nature() {
+    online.style.color = "rgb(198, 255, 123)";
+    body.style.backgroundColor = "rgb(67, 82, 47)";
+    users.style.backgroundColor = "rgb(67, 82, 47)";
+    users.style.borderColor = "rgb(178, 233, 107)"
+    main.style.backgroundColor = "rgb(178, 233, 107)";
+    lab.style.backgroundColor = "rgb(67, 82, 47)";
+    setLab.style.backgroundColor = "rgb(67, 82, 47)";
+    cover1.style.backgroundColor = "rgb(67, 82, 47)";
+    cover2.style.backgroundColor = "rgb(67, 82, 47)";
+    b1.style.backgroundColor = "rgb(178, 233, 107)";
+    b2.style.backgroundColor = "rgb(178, 233, 107)";
+    b3.style.backgroundColor = "rgb(178, 233, 107)";
+    b4.style.backgroundColor = "rgb(178, 233, 107)";
+    b5.style.backgroundColor = "rgb(178, 233, 107)";
+    b6.style.backgroundColor = "rgb(178, 233, 107)";
+    b7.style.backgroundColor = "rgb(178, 233, 107)";
+    ba.style.backgroundColor = "rgb(178, 233, 107)";
+    bb.style.backgroundColor = "rgb(178, 233, 107)";
+    bc.style.backgroundColor = "rgb(178, 233, 107)";
+    theme.style.backgroundColor = "rgb(178, 233, 107)"; 
+}
+
+function desert() {
+    online.style.color = "rgb(309, 199, 58)";
+    body.style.backgroundColor = "chocolate";
+    users.style.backgroundColor = "chocolate";
+    users.style.borderColor = "rgb(192, 178, 168)"
+    main.style.backgroundColor = "rgb(192, 178, 168)";
+    lab.style.backgroundColor = "rgba(184, 121, 3, 0.705)";
+    setLab.style.backgroundColor = "rgba(184, 121, 3, 0.705)";
+    cover1.style.backgroundColor = "rgb(192, 178, 168)";
+    cover2.style.backgroundColor = "rgb(192, 178, 168)";
+    b1.style.backgroundColor = "rgb(201, 198, 129)";
+    b2.style.backgroundColor = "rgb(201, 198, 129)";
+    b3.style.backgroundColor = "rgb(201, 198, 129)";
+    b4.style.backgroundColor = "rgb(201, 198, 129)";
+    b5.style.backgroundColor = "rgb(201, 198, 129)";
+    b6.style.backgroundColor = "rgb(201, 198, 129)";
+    b7.style.backgroundColor = "rgb(201, 198, 129)";
+    ba.style.backgroundColor = "rgb(201, 198, 129)";
+    bb.style.backgroundColor = "rgb(201, 198, 129)";
+    bc.style.backgroundColor = "rgb(201, 198, 129)";
+    theme.style.backgroundColor = "rgb(212, 196, 158)";
+}
+
+function snow() {
+    online.style.color = "rgb(36, 131, 255)";
+    body.style.backgroundColor = "snow";
+    users.style.backgroundColor = "snow";
+    users.style.borderColor = "rgb(166, 213, 255)"
+    main.style.backgroundColor = "rgb(166, 213, 255)";
+    lab.style.backgroundColor = "azure";
+    setLab.style.backgroundColor = "azure";
+    cover1.style.backgroundColor = "rgb(166, 213, 255)";
+    cover2.style.backgroundColor = "rgb(166, 213, 255)";
+    b1.style.backgroundColor = "rgb(165, 225, 253)";
+    b2.style.backgroundColor = "rgb(165, 225, 253)";
+    b3.style.backgroundColor = "rgb(165, 225, 253)";
+    b4.style.backgroundColor = "rgb(165, 225, 253)";
+    b5.style.backgroundColor = "rgb(165, 225, 253)";
+    b6.style.backgroundColor = "rgb(165, 225, 253)";
+    b7.style.backgroundColor = "rgb(165, 225, 253)";
+    ba.style.backgroundColor = "rgb(165, 225, 253)";
+    bb.style.backgroundColor = "rgb(165, 225, 253)";
+    bc.style.backgroundColor = "rgb(165, 225, 253)";
+    theme.style.backgroundColor = "rgb(219, 214, 255)";
+}
+
+function mary() {
+    online.style.color = "rgb(255, 160, 239)";
+    body.style.backgroundColor = "#612e9c";
+    users.style.backgroundColor = "#822a8e";
+    users.style.borderColor = "#f9a4fb"
+    main.style.backgroundColor = "#f9a4fb";
+    lab.style.backgroundColor = "#822a8e";
+    setLab.style.backgroundColor = "#822a8e";
+    cover1.style.backgroundColor = "#f9a4fb";
+    cover2.style.backgroundColor = "#f9a4fb";
+    b1.style.backgroundColor = "#9d73af";
+    b2.style.backgroundColor = "#9d73af";
+    b3.style.backgroundColor = "#9d73af";
+    b4.style.backgroundColor = "#9d73af";
+    b5.style.backgroundColor = "#9d73af";
+    b6.style.backgroundColor = "#9d73af";
+    b7.style.backgroundColor = "#9d73af";
+    ba.style.backgroundColor = "#9d73af";
+    bb.style.backgroundColor = "#9d73af";
+    bc.style.backgroundColor = "#9d73af";
+    theme.style.backgroundColor = "rgb(255, 181, 233)";
+}
+
+
+ planet.onclick = function (){
+    theme.style.display ="none";
+    planets();
+    localStorage.setItem("color","planets");
+}
+natur.onclick = function (){
+    theme.style.display ="none";
+    nature();
+    localStorage.setItem("color","nature");
+}
+deser.onclick = function (){
+    theme.style.display ="none";
+    desert();
+    localStorage.setItem("color","desert");
+}
+sno.onclick = function (){
+    theme.style.display ="none";
+    snow();
+    localStorage.setItem("color","snow");
+}
+mar.onclick = function (){
+    theme.style.display ="none";
+    mary();
+    localStorage.setItem("color","mary");
+}
+document.body.onload =()=>{
+    if (localStorage.getItem("color") == "planets") {
+        planets();
+    }
+    if (localStorage.getItem("color") == "nature") {
+        nature();
+    }
+    if (localStorage.getItem("color") == "desert") {
+        desert();
+    }
+    if (localStorage.getItem("color") == "snow") {
+        snow();
+    }
+    if (localStorage.getItem("color") == "mary") {
+        mary();
+    }
+    user.value = localStorage.getItem("firstname");
+}
+let user = document.getElementById("user");
+
+user.value = localStorage.getItem("firstname");
+       setInterval(()=>{
+    user.value = localStorage.getItem("firstname");
+});
+  
+
+let postForm = document.getElementById("postForm");
+postForm.onsubmit = (form)=>{
+form.preventDefault();
+
+let formData = new FormData(postForm);
+
+fetch("http://localhost/api/rooz/add.php", {
+method : "POST",
+body : formData
+}).then(response => response.json()).then(data =>{
+console.log(data);
+});
+
+    localStorage.setItem("text", document.getElementById("text").value);
+    document.getElementById("text").value = "";
+
+}
+
+bc.onclick = ()=>{
+    document.getElementById("text").value = localStorage.getItem("text");
+}
